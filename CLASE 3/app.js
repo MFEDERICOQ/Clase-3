68817698d9e9c52c1c1b149af6b6198b51a903af
mfedericoq <martinfquiroga@gmail.com>
2023-03-09 19:02:06 -0300

    Modificacion clase 3

--- CLASE 3/app.js
+++ CLASE 3/app.js
@@ -1,4 +1,5 @@
 //SALUDO PRESENTACION
 let person = prompt("Hola, ¿cuál es su nombre?");
-alert("Hola" + " " + person + ", bienvenido/a a mi perfil");
+document.getElementById("myName");
+myName.innerHTML = person;
 
